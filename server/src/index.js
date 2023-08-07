b572b5bf7f0b2fc7e28a5a55f17516a41a8d2b83
MagicaL <kawai.magar7@gmail.com>
2023-08-07 19:15:16 +0545

    Setting server models,routers and controller for Members

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 require('dotenv').config()
 const userRoute=require('./routes/user')
+const memberRoute=require('./routes/member')
 const connection = require('./db/connection')
 const Users = require('./models/user')
+const Member = require('./models/member')
 connection()
 const app = express()
 
@@ -12,6 +14,7 @@ const port = 4000
 app.use(express.json())
 app.use(cors())
 app.use("/",userRoute)
+app.use("/",memberRoute)
  
 
  
--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -8,6 +8,9 @@ router.post('/register', userController.registerUser)
 router.post('/login', userController.login)
 router.post('/change-password/:id', userController.changePassword)
 router.put('/users/:id', userController.changeUserDetails)
+
+
+
  
 
 
