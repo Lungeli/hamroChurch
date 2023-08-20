2c3521548cd6a8c51ef8a0f8385d08ae01e07f90
Lungeli <kawai.magar7@gmail.com>
2023-08-20 16:16:31 +0545

    Working on dashboard- total member count

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation';
 import { Button, Col, Row, Statistic } from 'antd';
 
@@ -10,10 +11,23 @@ import { useSelector } from 'react-redux';
 
 const Dashboard = () => {
   const router = useRouter()
-
+  const [totalCount, setTotalCount] = useState(0)
+  const fetchMembersDetails = async(page=1, size=10) => {
+    const res = await fetch(`http://localhost:4000/member`)
+    const data = await res.json()
+    console.log(data)
+ 
+    setTotalCount(data.count)
+}
+useEffect(()=>{
+    fetchMembersDetails()
+},[])
   const {userDetails} = useSelector(state=>state.users)
   const handleMember= () => {
     router.push('/members')
+  }
+  const viewMember= () => {
+    router.push('/members/view-member')
   }
     return(
       <>
@@ -22,12 +36,14 @@ const Dashboard = () => {
         <div style={{margin:'10px' ,backgroundColor:'lightgrey', padding:'5px', width:'200px'}}>
         <Row gutter={16} >
           <Col span={12}>
-      <Statistic title="Active Users" value={112893} />
+      <Statistic title="Total Members" value={totalCount} />
         </Col>
         </Row>
         </div>
 
-        <div><br/> <br/> <button onClick={handleMember}>Add Member</button></div>
+        <div><br/> <br/> <button onClick={handleMember}>Add Member</button>
+        <button onClick={viewMember}>View All Member</button>
+        </div>
      
           </>
     
