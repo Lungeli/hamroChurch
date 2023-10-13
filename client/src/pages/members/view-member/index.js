665b888cf066586f6f1c2483b043af76a7b06ff1
Lungeli <kawai.magar7@gmail.com>
2023-10-14 00:44:37 +0545

    Updaing View Member page with View option to show full details of user

--- client/src/pages/members/view-member/index.js
+++ client/src/pages/members/view-member/index.js
@@ -1,47 +1,116 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
-import { Skeleton } from 'antd';
-import Table from '../../../components/Table';
-import { Pagination } from 'antd';
-function index() {
-    const [members, setMembers] = useState([])
+import { useEffect, useState } from "react";
+import { Table, Button, Space, Modal } from "antd";
+import { EditOutlined, DeleteOutlined, EyeOutlined } from "@ant-design/icons";
 
-    const [totalCount, setTotalCount] = useState(0)
+const MembersPage = () => {
+  const [members, setMembers] = useState([]);
+  const [selectedMember, setSelectedMember] = useState(null);
 
-     const fetchMembersDetails = async(page=1, size=10) => {
-        const res = await fetch(`http://localhost:4000/member`)
-        const data = await res.json()
-        console.log(data)
-        setMembers(data.memberList)
-        setTotalCount(data.count)
-    }
-    useEffect(()=>{
-        fetchMembersDetails()
-    },[])
+  useEffect(() => {
+    // Fetch data from your API endpoint
+    fetch("http://localhost:4000/member")
+      .then((response) => response.json())
+      .then((data) => {
+        setMembers(data.memberList);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  const handleEdit = (record) => {
+    console.log("Edit:", record);
+  };
+
+  const handleDelete = (record) => {
+    console.log("Delete:", record);
+  };
+
+  const handleView = (record) => {
+    setSelectedMember(record);
+  };
+
+  const closeModal = () => {
+    setSelectedMember(null);
+  };
+
+  const columns = [
+    {
+      title: "Full Name",
+      dataIndex: "fullName",
+      key: "fullName",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+    {
+      title: "Actions",
+      key: "actions",
+      render: (text, record) => (
+        <Space>
+          <Button
+            type="primary"
+            icon={<EditOutlined />}
+            onClick={() => handleEdit(record)}
+          >
+            Edit
+          </Button>
+          <Button
+            type="danger"
+            icon={<DeleteOutlined />}
+            onClick={() => handleDelete(record)}
+          >
+            Delete
+          </Button>
+          <Button
+            type="default"
+            icon={<EyeOutlined />}
+            onClick={() => handleView(record)}
+          >
+            View
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
   return (
     <div>
-        {members.length>0 ? (
-            <div>
-            <div><input/></div>
-           {
-            members.map((item,id)=>{
-            return (
-                <div style={{margin:'10px' ,backgroundColor: id%5==0 ? 'grey':'lightgrey', padding:'5px', width:'200px'}}>
-                    {item.toString()}
-                    {item.fullName}
-                    </div>
-            )}
-            )
-            }
-            <Pagination 
-            showSizeChanger
-            onChange={(page ,size)=>fetchMemberDetails(page, size)} defaultCurrent={1} total={44} />
-            </div>): <Skeleton />}
-        
+      <h1>Members</h1>
+      <Table columns={columns} dataSource={members} />
 
-        {members.length>0 ? <Table fetchMembersDetails={fetchMembersDetails} members={members}/> : <Skeleton/>}
+      <Modal
+        title="User Details"
+        visible={selectedMember !== null}
+        onCancel={closeModal}
+        footer={[
+          <Button key="back" onClick={closeModal}>
+            Close
+          </Button>
+        ]}
+      >
+        {selectedMember && (
+          <div>
+            <p><strong>Full Name:</strong> {selectedMember.fullName}</p>
+            <p><strong>Email:</strong> {selectedMember.email}</p>
+            <p><strong>Address:</strong> {selectedMember.address}</p>
+            {/* Add more fields as needed */}
+            <p><strong>Phone Number:</strong> {selectedMember.phoneNumber}</p>
+            <p><strong>Date of Birth:</strong> {selectedMember.dob}</p>
+            <p><strong>Gender:</strong> {selectedMember.gender}</p>
+            <p><strong>Marital Status:</strong> {selectedMember.maritalStatus}</p>
+          </div>
+        )}
+      </Modal>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default MembersPage;
